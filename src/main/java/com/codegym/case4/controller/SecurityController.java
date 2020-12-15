66ac571258bcb036bd2c3e323fe6016ef37b6c8a
toni123654123 <tn1061999@gmail.com>
2020-12-15 08:24:07 +0700

    homeClient

--- src/main/java/com/codegym/case4/controller/SecurityController.java
+++ src/main/java/com/codegym/case4/controller/SecurityController.java
@@ -1,10 +1,15 @@
 package com.codegym.case4.controller;
 
+import com.codegym.case4.model.Book;
 import com.codegym.case4.model.Role;
 import com.codegym.case4.model.User;
-import com.codegym.case4.service.role.IRoleService;
-import com.codegym.case4.service.user.IUserService;
+import com.codegym.case4.service.Book.IBookService;
+import com.codegym.case4.service.Role.IRoleService;
+import com.codegym.case4.service.User.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,28 +19,52 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @Controller
 public class SecurityController {
+    @Autowired
+    private IBookService bookService;
 
     @Autowired
     private IUserService userService;
 
     @Autowired
     private IRoleService roleService;
+
     @Autowired
     PasswordEncoder passwordEncoder;
 
-    @RequestMapping("/user")
-    public String userInfo() {
-        return "userInfoPage";
+    @RequestMapping("/")
+    public ModelAndView listBooks(@RequestParam("s") Optional<String> s, @PageableDefault(size = 10) Pageable pageable) {
+        Page<Book> books;
+        if (s.isPresent()) {
+            books = bookService.findAllByTitleContainingAndDeletedIsFalse(s.get(), pageable);
+        } else {
+            books = bookService.findAll(pageable);
+        }
+        ModelAndView modelAndView = new ModelAndView("/homeClient");
+        modelAndView.addObject("books", books);
+        return modelAndView;
+    }
+
+    @RequestMapping("/client")
+    public ModelAndView listBooksC(@RequestParam("s") Optional<String> s, @PageableDefault(size = 10) Pageable pageable) {
+        Page<Book> books;
+        if (s.isPresent()) {
+            books = bookService.findAllByTitleContainingAndDeletedIsFalse(s.get(), pageable);
+        } else {
+            books = bookService.findAll(pageable);
+        }
+        ModelAndView modelAndView = new ModelAndView("/homeClient");
+        modelAndView.addObject("books", books);
+        return modelAndView;
     }
 
     @RequestMapping("/admin")
     public String adminPage() {
-
-        return "adminPage";
+        return "redirect:/admin/book";
 
     }
     @GetMapping("/create")
@@ -49,7 +78,7 @@ public class SecurityController {
     public ModelAndView saveCustomer(@Validated @ModelAttribute("user") User user, BindingResult bindingResult) {
         ModelAndView modelAndView = new ModelAndView("/create");
         if (!bindingResult.hasFieldErrors()) {
-            Role role = roleService.findRoleByroleName("ROLE_USER");
+            Role role = roleService.findRoleByRoleName("ROLE_USER");
             Set<Role> roles = new HashSet<>();
             roles.add(role);
             user.setRoles(roles);
@@ -63,22 +92,14 @@ public class SecurityController {
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String loginPage() {
-
         return "loginPage";
     }
 
 
-    @RequestMapping("/")
-    public String homePage() {
-        return "welcomePage";
-    }
-
     @RequestMapping(value = "/logoutSuccessful", method = RequestMethod.GET)
     public String logoutSuccessfulPage(Model model) {
         model.addAttribute("title", "Logout");
-        return "welcomePage";
+        return "logoutSuccessfulPage";
     }
-
-
 
 }
