0fd6a7193e14574f6885de435ab4de21468007b8
toni123654123 <tn1061999@gmail.com>
2020-12-16 10:07:21 +0700

    oke

--- src/main/java/com/codegym/case4/Case4Application.java
+++ src/main/java/com/codegym/case4/Case4Application.java
@@ -1,26 +1,22 @@
 package com.codegym.case4;
 
 import com.codegym.case4.model.Role;
-import com.codegym.case4.service.role.IRoleService;
-import com.codegym.case4.service.user.IUserService;
+import com.codegym.case4.service.Role.IRoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
 
 @SpringBootApplication
 public class Case4Application {
-    @Autowired
-    private IUserService userService;
+
 
     @Autowired
     private IRoleService roleService;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+
     @PostConstruct
     public void init(){
         List<Role> roles = (List<Role>) roleService.findAll();
@@ -32,10 +28,9 @@ public class Case4Application {
             roleUser.setRoleName("ROLE_USER");
             roleService.save(roleUser);
         }
-
     }
     public static void main(String[] args) {
         SpringApplication.run(Case4Application.class, args);
-
     }
+
 }
